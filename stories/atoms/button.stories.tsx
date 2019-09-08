61fe1cc0a7bb086a29ca2b6a9138299b1e8e92ba
LitenApe <sonvo95@outlook.com>
2019-09-08 13:27:37 +0200

    Add button

--- stories/atoms/button.stories.tsx
+++ stories/atoms/button.stories.tsx
@@ -4,7 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { text, boolean } from '@storybook/addon-knobs';
 
-import { Button } from '../../src/atoms/buttons';
+import { CenterContent, } from "../utils";
+import { Button, } from "../../src";
 
 const stories = storiesOf("atoms/Button", module);
 
@@ -12,12 +13,52 @@ stories.addParameters({
   info: { inline: true }
 })
 
+stories.addDecorator(CenterContent);
+
 stories.add(
   'Default Button',
   () => (
     <Button
       text={text("Button text", 'text')}
       disabled={boolean("Disabled", false)}
+      rounded={boolean("Round", false)}
+      accent={boolean("Accent", false)}
+      onClick={action('Clicked')}
+    />
+  ));
+
+stories.add(
+  'Disabled Button',
+  () => (
+    <Button
+      text={text("Button text", 'text')}
+      disabled={boolean("Disabled", true)}
+      rounded={boolean("Round", false)}
+      accent={boolean("Accent", false)}
+      onClick={action('Clicked')}
+    />
+  ));
+
+stories.add(
+  'Round Button',
+  () => (
+    <Button
+      text={text("Button text", 'text')}
+      disabled={boolean("Disabled", false)}
+      rounded={boolean("Round", true)}
+      accent={boolean("Accent", false)}
+      onClick={action('Clicked')}
+    />
+  ));
+
+stories.add(
+  'Accent Button',
+  () => (
+    <Button
+      text={text("Button text", 'text')}
+      disabled={boolean("Disabled", false)}
+      rounded={boolean("Round", false)}
+      accent={boolean("Accent", true)}
       onClick={action('Clicked')}
     />
   ));
