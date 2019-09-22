a105eb971654b4859351d3da03b61662c51bdf6c
LitenApe <sonvo95@outlook.com>
2019-09-22 20:17:04 +0200

    chore: Add a11y and update dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 
 const req = require.context('../stories', true, /\.stories\.tsx$/);
 function loadStories() {
@@ -9,6 +10,7 @@ function loadStories() {
 
 addDecorator(withInfo);
 addDecorator(withKnobs);
+addDecorator(withA11y);
 addParameters({ info: { inline: true } } );
 
 configure(loadStories, module);
--- stories/utils.tsx
+++ stories/utils.tsx
@@ -1,11 +1,10 @@
 import React, { ReactElement, } from "react";
-import { RenderFunction, } from "@storybook/react";
 
 const horizontalCenter = {
   display: "flex",
   justifyContent: "center",
 };
 
-export const CenterContent = (storyFn: RenderFunction): ReactElement => (
+export const CenterContent = (storyFn: () => JSX.Element): ReactElement => (
   <div style={horizontalCenter}>{storyFn()}</div>
 );
