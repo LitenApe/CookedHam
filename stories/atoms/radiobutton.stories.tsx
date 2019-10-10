25f3834b4dc0a739e03f7b7172e9798dffa4c8b2
LitenApe <sonvo95@outlook.com>
2019-10-10 22:34:09 +0200

    style: Use native radio
    \#5

--- stories/atoms/radiobutton.stories.tsx
+++ stories/atoms/radiobutton.stories.tsx
@@ -7,7 +7,7 @@ import { RadioButton, } from "../../src/atoms/radiobutton";
 
 const stories = storiesOf("atoms/RadioButton", module);
 
-stories.add("Default RadioButton", (): ReactElement => (
+stories.add("Default", (): ReactElement => (
   <RadioButton
     label={text("Label", "label")}
     checked={boolean("Checked", false)}
@@ -15,38 +15,5 @@ stories.add("Default RadioButton", (): ReactElement => (
     value={text("Value", "value")}
     disabled={boolean("Disabled", false)}
     reversed={boolean("Reversed", false)}
-  />
-));
-
-stories.add("Checked RadioButton", (): ReactElement => (
-  <RadioButton
-    label={text("Label", "label")}
-    checked={boolean("Checked", true)}
-    onChange={action("State change requested")}
-    value={text("Value", "value")}
-    disabled={boolean("Disabled", false)}
-    reversed={boolean("Reversed", false)}
-  />
-));
-
-stories.add("Disabled RadioButton", (): ReactElement => (
-  <RadioButton
-    label={text("Label", "label")}
-    checked={boolean("Checked", false)}
-    onChange={action("State change requested")}
-    value={text("Value", "value")}
-    disabled={boolean("Disabled", true)}
-    reversed={boolean("Reversed", false)}
-  />
-));
-
-stories.add("Reversed RadioButton", (): ReactElement => (
-  <RadioButton
-    label={text("Label", "label")}
-    checked={boolean("Checked", false)}
-    onChange={action("State change requested")}
-    value={text("Value", "value")}
-    disabled={boolean("Disabled", false)}
-    reversed={boolean("Reversed", true)}
   />
 ));
