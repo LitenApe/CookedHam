d7250316a58aff825e3a8728d2a571cfc03da074
LitenApe <sonvo95@outlook.com>
2019-10-13 10:25:55 +0200

    feat: Add DescriptionList Component
    Add DescriptionList to provide users with an elegant solution to provide information.
    
    \#7

---
+++ .storybook/stories/atoms/description-list.stories.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+
+import { DescriptionList, CompactDescriptionList } from "../../../src";
+
+const stories = storiesOf("Atoms/DescriptionList", module);
+
+stories.add("DescriptionList", () => (
+  <DescriptionList items={[
+    {
+      label: [
+        "Braun Nurnberg",
+        "Heliopan",
+        "Keiser Fototechnick"
+      ],
+      description: "Manufacturer of photo equipments",
+    },
+    {
+      label: "Braun Nurnberg",
+      description: [
+        "Canon",
+        "Casio",
+        "Fujifilm",
+        "Fujinon",
+        "Hasselblad"
+      ],
+    },
+    {
+      label: "Heliopan",
+      description: "IMAX",
+    },
+    {
+      label: "Keiser Fototechnick",
+      description: [
+        "Kodak",
+        "Leica",
+        "Nikon",
+        "Olympus",
+        "Panasonic",
+        "Pentax",
+        "Polaroid",
+        "Zeiss"
+      ],
+    },
+    {
+      label: "Former producers of cameras or lenses and filters",
+      description: [
+        "Bronica",
+        "Bell & Howell",
+        "Ducati",
+        "Honeywell",
+        "Konica"
+      ],
+    }
+  ]}
+  />
+));
+
+stories.add("CompactDescriptionList", () => (
+  <CompactDescriptionList items={[
+    { label: "Blackmagic Design", description: "Australia", },
+    { label: "Phase One", description: "Denmark", style: "underline", },
+    { label: "Leica", description: "Germany", },
+    { label: "Canon", description: "Japan", },
+    { label: "Fujifilm", description: "Japan", highlight: true, },
+    { label: "Nikon", description: "Japan", },
+    { label: "Olympus", description: "Japan", },
+    { label: "Ricoh", description: "Japan", style: "bold", },
+    { label: "Sigma", description: "Japan", },
+    { label: "Sony", description: "Japan", style: "line-through", },
+    { label: "Hasselblad", description: "Sweden", }
+  ]}
+  />
+));
