7b9cf93ab48724279d8c81667c511e1670d20ae9
LitenApe <sonvo95@outlook.com>
2021-06-16 21:32:57 +0200

    refactor: remove else block in useId

--- src/utils/useId.ts
+++ src/utils/useId.ts
@@ -1,16 +1,13 @@
 import { useMemo } from 'react';
 
-const _used: { [key: string]: number } = {};
+const _usedIds: { [key: string]: number } = {};
 
 function useId(prefix: string = 'kitchen') {
   const id = useMemo(() => {
-    if (_used[prefix] === undefined) {
-      _used[prefix] = 1;
-      return `${prefix}-1`;
-    } else {
-      _used[prefix] += 1;
-      return `${prefix}-${_used[prefix]}`;
+    if (_usedIds[prefix] === undefined) {
+      _usedIds[prefix] = 0;
     }
+    return `${prefix}-${++_usedIds[prefix]}`
   }, [prefix]);
 
   return id;
