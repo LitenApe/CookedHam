867b474b854818ca63a105b4266dfd3cda14c6e9
LitenApe <sonvo95@outlook.com>
2021-06-25 17:16:29 +0200

    refactor: use createElement instead

--- src/atoms/checkbox/Checkbox.ts
+++ src/atoms/checkbox/Checkbox.ts
@@ -1,7 +1,8 @@
+import { createElement } from 'react';
 import Input, { InputProps } from '../input/Input';
 
 export type CheckboxProps = Omit<InputProps, 'type'>;
 
 export default function Checkbox(props: CheckboxProps) {
-  return Input({ ...props, type: 'checkbox' });
+  return createElement(Input, { ...props, type: 'checkbox' });
 }
--- src/atoms/radio/Radio.ts
+++ src/atoms/radio/Radio.ts
@@ -1,7 +1,8 @@
+import { createElement } from 'react';
 import Input, { InputProps } from '../input/Input';
 
 export type RadioProps = Omit<InputProps, 'type'>;
 
 export default function Radio(props: RadioProps) {
-  return Input({ ...props, type: 'radio' });
+  return createElement(Input, { ...props, type: 'radio' });
 }
