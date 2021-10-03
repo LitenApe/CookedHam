b01d629be0cb28ebdeb75bb4f957a919f3ffd22c
Son Thanh Vo <sonvo95@outlook.com>
2021-10-03 15:08:31 +0200

    feat: use framer motion on accordion

--- src/atoms/accordion/Accordion.stories.tsx
+++ src/atoms/accordion/Accordion.stories.tsx
@@ -13,6 +13,11 @@ Accordion.args = {
   ...Accordion.args,
   children: [
     <Component.Header key="accordion_title">Title</Component.Header>,
-    <Component.Panel key="accordion_content">lorem ipsum</Component.Panel>,
+    <Component.Panel key="accordion_content">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
+      voluptatum ipsa deserunt ex assumenda? Officiis minima cum possimus
+      deleniti, delectus veritatis laboriosam aspernatur vero nemo. Dolores,
+      reiciendis. Libero, autem adipisci.
+    </Component.Panel>,
   ],
 };
--- src/atoms/accordion/Accordion.ts
+++ src/atoms/accordion/Accordion.ts
@@ -1,9 +1,10 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import {
   createElement,
   createContext,
   useState,
-  ComponentProps,
   useContext,
+  ComponentProps,
 } from 'react';
 import useId from '../../utils/hooks/useId';
 
@@ -25,7 +26,7 @@ function useAccordion(): Context {
   return context;
 }
 
-function Accordion({ ...rest }: ComponentProps<'div'>) {
+function Accordion(props: ComponentProps<'div'>) {
   const id = useId('accordion');
   const [open, setOpen] = useState(false);
 
@@ -38,7 +39,7 @@ function Accordion({ ...rest }: ComponentProps<'div'>) {
     {
       value: { id, open, onClick },
     },
-    createElement('div', rest)
+    createElement('div', props)
   );
 }
 
@@ -52,15 +53,43 @@ Accordion.Header = function Header(props: ComponentProps<'button'>) {
     ...props,
   });
 };
-
-Accordion.Panel = function Panel(props: ComponentProps<'section'>) {
+Accordion.Panel = function Panel({
+  children,
+  ...rest
+}: ComponentProps<'section'>) {
   const { id, open } = useAccordion();
-  return createElement('section', {
-    id: `${id}_content`,
-    'aria-labelledby': `${id}_button`,
-    hidden: !open,
-    ...props,
-  });
+
+  return createElement(
+    'section',
+    {
+      ...rest,
+      id: `${id}_content`,
+      'aria-labelledby': `${id}_button`,
+      'aria-hidden': !open,
+    },
+    createElement(
+      AnimatePresence,
+      { initial: false },
+      open
+        ? createElement(
+            motion.section,
+            {
+              key: `${id}_content`,
+              initial: 'collapsed',
+              animate: 'open',
+              exit: 'collapsed',
+              style: { overflow: 'hidden' },
+              variants: {
+                open: { height: 'auto' },
+                collapsed: { height: 0 },
+              },
+              transition: { duration: 0.3, type: 'spring' },
+            },
+            children
+          )
+        : null
+    )
+  );
 };
 
 export default Accordion;
