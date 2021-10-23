26e6c5cee6880832693a6dcd9bb40bbb8cf323c4
LitenApe <sonvo95@outlook.com>
2021-10-23 13:18:13 +0200

    refactor: move children to template

--- src/atoms/radio_group/RadioGroup.stories.tsx
+++ src/atoms/radio_group/RadioGroup.stories.tsx
@@ -3,28 +3,31 @@ import { action } from '@storybook/addon-actions';
 import { RadioGroup as Component } from '.';
 import Radio from '../radio/Radio';
 import Label from '../label/Label';
+import { Legend } from '../legend';
 
 export default {
   title: 'Atom/RadioGroup',
   component: Component,
 } as Meta;
 
-const Template: Story = (args) => <Component {...args} />;
+const Template: Story = (args) => (
+  <Component {...args}>
+    <Legend>Bicycle brand</Legend>
+    <Radio key="radio-1" value="trek" />
+    <Label key="radio-1-label">Trek</Label>
+    <Radio key="radio-2" value="specialized" />
+    <Label key="radio-2-label">Specialized</Label>
+    <Radio key="radio-3" value="cannondale" />
+    <Label key="radio-3-label">Cannondale</Label>
+    <Radio key="radio-4" value="canyon" />
+    <Label key="radio-4-label">Canyon</Label>
+  </Component>
+);
 
 export const RadioGroup = Template.bind({});
 RadioGroup.storyName = 'RadioGroup';
 RadioGroup.args = {
   ...RadioGroup.args,
   name: 'bike-manufacturer',
-  children: [
-    <Radio key="radio-1" value="trek" />,
-    <Label key="radio-1-label">Trek</Label>,
-    <Radio key="radio-2" value="specialized" />,
-    <Label key="radio-2-label">Specialized</Label>,
-    <Radio key="radio-3" value="cannondale" />,
-    <Label key="radio-3-label">Cannondale</Label>,
-    <Radio key="radio-4" value="canyon" />,
-    <Label key="radio-4-label">Canyon</Label>,
-  ],
   onChange: action('value changed'),
 };
