56e128f4582bf7a200300ab7f61844fc4635b46a
LitenApe <sonvo95@outlook.com>
2021-10-23 09:33:55 +0200

    chore: remove children from story arg table

--- src/atoms/error_boundary/ErrorBoundary.stories.tsx
+++ src/atoms/error_boundary/ErrorBoundary.stories.tsx
@@ -4,22 +4,26 @@ import { ErrorBoundary as Component } from '.';
 export default {
   title: 'Atom/ErrorBoundary',
   component: Component,
+  argTypes: {
+    fallback: {
+      name: 'fallback',
+      description: 'ReactElement to render if children throws an Error',
+    },
+  },
 } as Meta;
-
-const Template: Story = ({ children, ...rest }) => (
-  <Component fallback={() => <></>} {...rest}>
-    {children}
-  </Component>
-);
 
 function ErrorThrower(): JSX.Element {
   throw new Error('Error thrown to trigger fallback');
 }
 
+const Template: Story = ({ children, ...rest }) => (
+  <Component fallback={() => <p>Fallback component rendered</p>} {...rest}>
+    <ErrorThrower />
+  </Component>
+);
+
 export const ErrorBoundary = Template.bind({});
 ErrorBoundary.storyName = 'ErrorBoundary';
 ErrorBoundary.args = {
   ...ErrorBoundary.args,
-  fallback: () => <p>Fallback component rendered</p>,
-  children: <ErrorThrower />,
 };
