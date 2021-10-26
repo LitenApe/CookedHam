cbad7206ac5a49b9dca5fca0fc08df6d8ea8f4e3
LitenApe <sonvo95@outlook.com>
2021-10-26 19:35:22 +0200

    feat: add isUndefined and isNull

---
+++ src/utils/functions/__tests__/isUndefined.spec.ts
@@ -0,0 +1,26 @@
+import { isUndefined } from '../isUndefined';
+
+describe('utility function isUndefined', () => {
+  test('returns "true" if value is null', () => {
+    expect(isUndefined(undefined)).toBe(true);
+  });
+
+  test('returns "false" if value is not null', () => {
+    expect(isUndefined('')).toBe(false);
+    expect(isUndefined('123')).toBe(false);
+
+    expect(isUndefined(0)).toBe(false);
+    expect(isUndefined(1)).toBe(false);
+
+    expect(isUndefined({})).toBe(false);
+    expect(isUndefined({ foo: 'bar' })).toBe(false);
+
+    expect(isUndefined([])).toBe(false);
+    expect(isUndefined([1, 2])).toBe(false);
+
+    expect(isUndefined(true)).toBe(false);
+    expect(isUndefined(false)).toBe(false);
+
+    expect(isUndefined(null)).toBe(false);
+  });
+});
