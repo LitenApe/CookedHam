bc1cded07601e18774e5b26151281245d67f8f9e
Son Thanh Vo <sonvo95@outlook.com>
2021-10-30 12:22:09 +0200

    chore: add backdrop story description

--- src/atoms/backdrop/Backdrop.stories.tsx
+++ src/atoms/backdrop/Backdrop.stories.tsx
@@ -4,6 +4,22 @@ import { Backdrop as Component } from '.';
 export default {
   title: 'Atom/Backdrop',
   component: Component,
+  parameters: {
+    componentSubtitle:
+      'An overlay to highlight content which needs the users attention',
+    docs: {
+      description: {
+        component: `
+    The overlay is a simple "backdrop" to hide away the current
+    content temporarily. Commonly used to highlight images when
+    clicked on or to hightlight content which needs the users
+    attention. Can also be found in combination with modals,
+    where content is hidden away until the user chooses to see
+    the information
+        `,
+      },
+    },
+  },
 } as Meta;
 
 const Template: Story = (args) => <Component {...args} />;
