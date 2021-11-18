e3cb969c963b4024ecab69066dfc4f4d5abaa56f
Son Thanh Vo <sonvo95@outlook.com>
2021-11-18 20:13:03 +0100

    feat: add input field

---
+++ src/molecules/input_field/InputField.ts
@@ -0,0 +1,22 @@
+import { createElement, ForwardedRef, forwardRef } from 'react';
+import { BaseInputProps } from '../../atoms/base_input';
+import { Input } from '../../atoms/input';
+import { Label } from '../../atoms/label';
+import { useId } from '../../utils/hooks/useId';
+import { BaseField } from '../base_field';
+
+type Props = {
+  label: string;
+} & BaseInputProps<'input'>;
+
+function InputField(props: Props, ref: ForwardedRef<HTMLInputElement>) {
+  const { label, ...rest } = props;
+  const id = useId('input-field');
+
+  return createElement(BaseField, rest, [
+    createElement(Label, { key: `${id}_label` }, label),
+    createElement(Input, { ...rest, key: `${id}_component`, ref }),
+  ]);
+}
+
+export default forwardRef(InputField);
