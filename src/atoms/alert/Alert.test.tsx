c47922827a4a72c7036ce5b2460524425e4fdd66
Son Thanh Vo <sonvo95@outlook.com>
2022-03-10 15:20:52 +0100

    test: suppress mutation not wrapped in act

--- src/atoms/alert/Alert.test.tsx
+++ src/atoms/alert/Alert.test.tsx
@@ -1,7 +1,20 @@
 import { act, render, screen } from '@testing-library/react';
+
 import { Alert } from '.';
 
+// suppressing warnings caused by async
+// mount operation inside of component
+const originalError = console.error;
+
 describe('Alert general behavior', () => {
+  beforeAll(() => {
+    console.error = jest.fn();
+  });
+
+  afterAll(() => {
+    console.error = originalError;
+  });
+
   beforeEach(() => {
     jest.useFakeTimers();
   });
