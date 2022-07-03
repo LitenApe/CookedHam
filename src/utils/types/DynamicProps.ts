4d7c37392d1e9eb7db686da61ebc01fb0b7f84a0
Son Thanh Vo <sonvo95@outlook.com>
2022-07-03 18:36:08 +0200

    chore: upgrade storybook

--- src/utils/types/DynamicProps.ts
+++ src/utils/types/DynamicProps.ts
@@ -1,20 +1,24 @@
-import * as React from 'react';
+import type {
+  ComponentPropsWithoutRef,
+  ElementType,
+  JSXElementConstructor,
+} from 'react';
 
 export type PropsOf<
-  T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
-> = JSX.LibraryManagedAttributes<T, React.ComponentPropsWithoutRef<T>>;
+  T extends keyof JSX.IntrinsicElements | JSXElementConstructor<any>
+> = JSX.LibraryManagedAttributes<T, ComponentPropsWithoutRef<T>>;
 
-export type AsProps<T extends React.ElementType> = {
+export type AsProps<T extends ElementType> = {
   as?: T;
 };
 
 export type ExtendableProps<ExtendedProps, OverrideProps> = OverrideProps &
   Omit<ExtendedProps, keyof OverrideProps>;
 
-export type InheritableElementProps<
-  T extends React.ElementType,
+export type InheritableElementProps<T extends ElementType, P> = ExtendableProps<
+  PropsOf<T>,
   P
-> = ExtendableProps<PropsOf<T>, P>;
+>;
 
-export type DynamicProps<T extends React.ElementType> = AsProps<T> &
-  React.ComponentPropsWithoutRef<T>;
+export type DynamicProps<T extends ElementType> = AsProps<T> &
+  ComponentPropsWithoutRef<T>;
